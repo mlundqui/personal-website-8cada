ef8fc3d47926ca75e0ca5b570e95d2b8bd7f3b5b
Michael Lundquist <mlundqui@g.ucla.edu>
2025-07-18 19:51:20 -0700

    Adding Blog Header Link and page

--- components/header.jsx
+++ components/header.jsx
@@ -9,7 +9,8 @@ const navItems = [
     { linkText: 'Image CDN', href: '/image-cdn' },
     { linkText: 'Edge Function', href: '/edge' },
     { linkText: 'Blobs', href: '/blobs' },
-    { linkText: 'Classics', href: '/classics' }
+    { linkText: 'Classics', href: '/classics' },
+    {linkText: 'Blog', href: '/blog'}
 ];
 
 export function Header() {
